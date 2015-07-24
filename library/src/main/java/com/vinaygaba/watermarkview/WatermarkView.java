855211a7f472b618319ca6143411c15906e8a68a
vinay <vinaygaba@gmail.com>
2015-07-24 15:13:35 +0530

    -Renamed class name and deleted extra class

--- library/src/main/java/com/vinaygaba/watermarkview/WatermarkView.java
+++ library/src/main/java/com/vinaygaba/watermarkview/WatermarkView.java
@@ -14,89 +14,218 @@
  * limitations under the License.
  */
 
+
 package com.vinaygaba.watermarkview;
 
 import android.content.Context;
-import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.Nullable;
-import android.support.v4.util.Pair;
-import android.util.AttributeSet;
-import android.widget.ImageView;
+import android.util.Log;
 
-public class WatermarkView extends ImageView {
+public class WatermarkView {
 
-    private int mWatermark;
     static Context mContext;
-    private int mImageWidth;
-    private int mImageHeight;
-    private int mWatermarkWidth;
-    private int mWatermarkHeight;
+    private int x;
+    private int y;
+    private int srcWidth;
+    private int srcHeight;
+    private int watermarkWidth;
+    private int watermarkHeight;
 
-
-    public WatermarkView(Context context) {
-        this(context, null);
+    public WatermarkView(Context context){
+        mContext = context;
     }
 
-    public WatermarkView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        if (context != null) {
-            this.mContext = context;
-        } else {
-            this.mContext = getContext();
+    public Bitmap addWatermark(Bitmap src, String watermark,int size,int color,String typeFacePath,Position pos) {
+        srcWidth = src.getWidth();
+        Log.e("Src Width",srcWidth+"");
+        srcHeight = src.getHeight();
+        Log.e("Src Height",srcHeight+"");
+        Rect bounds = new Rect();
+        Shader shader = new LinearGradient(0, 0, 100, 0, Color.TRANSPARENT, color, Shader.TileMode.CLAMP);
+
+        Bitmap result = Bitmap.createBitmap(srcWidth, srcHeight, src.getConfig());
+
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(src, 0, 0, null);
+
+        Paint paint = new Paint();
+        paint.setTextSize(size);
+
+        if(typeFacePath != null && typeFacePath!="") {
+            Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), typeFacePath);
+            paint.setTypeface(typeface);
         }
 
-        init();
-        loadAttributes(attrs);
-        initDefaults();
+        paint.getTextBounds(watermark,0,watermark.length(),bounds);
+        watermarkHeight=bounds.height();
+        Log.e("Watermark Height",watermarkHeight+"");
+        watermarkWidth=bounds.width();
+        Log.e("Watermark Width",watermarkWidth+"");
+        paint.setAntiAlias(true);
+        paint.setShader(shader);
+        paint.setUnderlineText(false);
+
+        getWatermarkCoordinates(pos,srcWidth,srcHeight);
+
+        canvas.drawText(watermark,x , y, paint);
+        Log.e("X", x+"");
+        Log.e("Y", y+"");
+
+
+
+        return result;
     }
 
-    /**
-     * Initialize various views and variables
-     */
-    private void init() {
+    public Bitmap addWatermark(int src, String watermark,int size,int color,String typeFacePath,Position pos) {
 
+        Bitmap srcBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                src);
 
-    }
+        srcWidth = srcBitmap.getWidth();
+        Log.e("Src Width",srcWidth+"");
+        srcHeight = srcBitmap.getHeight();
+        Log.e("Src Height",srcHeight+"");
+        Rect bounds = new Rect();
+        Shader shader = new LinearGradient(0, 0, 100, 0, Color.TRANSPARENT, color, Shader.TileMode.CLAMP);
 
-    private void loadAttributes(@Nullable AttributeSet attrs) {
+        Bitmap result = Bitmap.createBitmap(srcWidth, srcHeight, srcBitmap.getConfig());
 
-        final TypedArray a = mContext.getTheme().obtainStyledAttributes(attrs,
-                R.styleable.WatermarkView, 0, 0);
-        try {
-            mWatermark = a.getResourceId(R.styleable.WatermarkView_watermark, 0);
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(srcBitmap, 0, 0, null);
 
+        Paint paint = new Paint();
+        paint.setTextSize(size);
+
+        if(typeFacePath != null && typeFacePath!="") {
+            Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), typeFacePath);
+            paint.setTypeface(typeface);
         }
-        finally {
-            a.recycle();
+
+        paint.getTextBounds(watermark,0,watermark.length(),bounds);
+
+        watermarkHeight=bounds.height();
+        watermarkWidth=bounds.width();
+
+        paint.setAntiAlias(true);
+        paint.setShader(shader);
+        paint.setUnderlineText(false);
+
+        getWatermarkCoordinates(pos, srcWidth, srcHeight);
+
+        canvas.drawText(watermark, x, y, paint);
+        Log.e("X", x + "");
+        Log.e("Y", y + "");
+
+        return result;
+    }
+
+    public Bitmap addWatermark(int src, int watermark,int wMarkWidth,int wMarkHeight,Position pos) {
+
+        Bitmap srcBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                src);
+
+        Bitmap tempWatermarkBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                watermark);
+
+        Bitmap watermarkBitmap = getResizedBitmap(tempWatermarkBitmap,wMarkHeight,wMarkWidth);
+
+        srcWidth = srcBitmap.getWidth();
+        srcHeight = srcBitmap.getHeight();
+
+        watermarkHeight=watermarkBitmap.getHeight();
+        watermarkWidth=watermarkBitmap.getWidth();
+
+
+        Bitmap result = Bitmap.createBitmap(srcWidth, srcHeight, srcBitmap.getConfig());
+
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(srcBitmap, 0, 0, null);
+        getWatermarkCoordinates(pos, srcWidth, srcHeight);
+        canvas.drawBitmap(watermarkBitmap,x,y-watermarkHeight,null);
+
+        return result;
+    }
+
+    public static Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+
+        return resizedBitmap;
+
+    }
+
+    public void getWatermarkCoordinates(Position pos,int w,int h){
+
+        switch(pos){
+
+            case TOPLEFT:
+                x = 0;
+                y = watermarkHeight;
+                break;
+            case TOPCENTER:
+                x = (srcWidth / 2) - (watermarkWidth / 2);
+                y = watermarkHeight;
+                break;
+            case TOPRIGHT:
+                x = srcWidth - watermarkWidth;
+                y = watermarkHeight;
+                break;
+
+            case MIDDLELEFT:
+                x = 0;
+                y = (srcHeight / 2) - (watermarkHeight / 2);
+                break;
+            case MIDDLECENTER:
+                Log.e("Entered", "Middlecenter");
+                x = (srcWidth / 2) - (watermarkWidth / 2);
+                y = (srcHeight / 2) - (watermarkHeight / 2);
+                break;
+            case MIDDLERIGHT:
+                x = srcWidth - watermarkWidth;
+                y = (srcHeight / 2) - (watermarkHeight / 2);
+                break;
+
+            case BOTTOMLEFT:
+                x = 0;
+                y = srcHeight;
+                break;
+            case BOTTOMCENTER:
+                x = (srcWidth / 2) - (watermarkWidth / 2);
+                y = srcHeight ;
+                break;
+            case BOTTOMRIGHT:
+                x = srcWidth - watermarkWidth;
+                y = srcHeight;
+                break;
+
+            case DIAGONAL:
+                x=0;
+                y=srcHeight;
+                break;
+
+            default:
+                break;
         }
-    }
-
-    private void initDefaults() {
-
-        mImageWidth = getWidth();
-        mImageHeight = getHeight();
-
 
     }
-
-    private void redrawViews() {
-        invalidate();
-        requestLayout();
-    }
-
-    @DrawableRes
-    public int getWatermark() {
-        return mWatermark;
-    }
-
-    public void setWatermark(@DrawableRes int waterMark) {
-        mWatermark = waterMark;
-        redrawViews();
-    }
-
-
-
 
 }
