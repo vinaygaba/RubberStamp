6dd7bba4144a50ff0b82fec23404e60c92cf72d4
Vinay Gaba <vinaygaba@gmail.com>
2017-05-12 00:03:47 -0700

    Wrote the sample app to go with the library (#32)
    * Made good progress on the sample app
    
    * Added more UI elements
    
    * Sample app close to complete

--- sample/src/main/java/com/vinaygaba/sample/MainActivity.java
+++ sample/src/main/java/com/vinaygaba/sample/MainActivity.java
@@ -6,16 +6,30 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 import com.vinaygaba.rubberstamp.RubberStamp;
 import com.vinaygaba.rubberstamp.RubberStampConfig;
-import com.vinaygaba.rubberstamp.RubberStampConfig.RubberStampConfigBuilder;
 import com.vinaygaba.rubberstamp.RubberStampPosition;
 
 import rx.Observable;
@@ -27,46 +41,148 @@ import rx.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Sample";
-    ImageView imageView;
+    private ImageView mImageView;
+    private RadioGroup mRadioGroup;
+    private Bitmap mBaseBitmap;
+    private Button mGenerateButton;
+    private SeekBar mAlphaSeekBar;
+    private SeekBar mRotationSeekBar;
+    private SeekBar mTextSizeSeekBar;
+    private Spinner mRubberStampPosition;
+    private LinearLayout mTextLayoutWrapper;
+    private TextView mTextColor, mTextBackgroundColor;
+    private EditText mRubberStampText;
+    private ColorPicker mColorPicker;
+    private RubberStamp mRubberStamp;
+    @ColorInt private int mTextColorValue = Color.RED;
+    @ColorInt private int mTextBackgroundColorValue = Color.WHITE;
+    private Switch mShaderSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        init();
+        setListeners();
+    }
 
-        Bitmap logo = BitmapFactory.decodeResource(getResources(),
-                R.drawable.logo);
-        Bitmap lenna = BitmapFactory.decodeResource(getResources(),
+    public void init() {
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mBaseBitmap = BitmapFactory.decodeResource(getResources(),
                 R.drawable.lenna);
+        mGenerateButton = (Button) findViewById(R.id.generateRubberStamp);
+        mAlphaSeekBar = (SeekBar) findViewById(R.id.alphaSeekBar);
+        mRotationSeekBar = (SeekBar) findViewById(R.id.rotationSeekBar);
+        mTextSizeSeekBar = (SeekBar) findViewById(R.id.textSizeSeekBar);
+        mRubberStampPosition = (Spinner) findViewById(R.id.rubberStampPositions);
+        mTextLayoutWrapper = (LinearLayout) findViewById(R.id.textLayoutWrapper);
+        mTextColor = (TextView) findViewById(R.id.textColor);
+        mTextBackgroundColor = (TextView) findViewById(R.id.textBackgroundColor);
+        mRubberStampText = (EditText) findViewById(R.id.rubberStampEditText);
+        mShaderSwitch = (Switch) findViewById(R.id.shaderSwitch);
+        mColorPicker = new ColorPicker(this, 255, 0, 0);
+        mRubberStamp = new RubberStamp(this);
+    }
 
-        RubberStamp rubberStamp = new RubberStamp(this);
-        int[] rainbow = {Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA};
-        Shader shader = new LinearGradient(0, 0, 0, logo.getWidth(), rainbow,
-                null, Shader.TileMode.MIRROR);
+    public void setListeners() {
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.textRubberStamp) {
+                    mTextLayoutWrapper.setVisibility(View.VISIBLE);
+                } else {
+                    mTextLayoutWrapper.setVisibility(View.GONE);
+                }
+            }
+        });
 
-        Matrix matrix = new Matrix();
-        matrix.setRotate(90);
-        shader.setLocalMatrix(matrix);
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateRubberStamp();
+            }
+        });
 
-        RubberStampConfig config = new RubberStampConfigBuilder()
-                .base(lenna)
-                .rubberStamp("Watermark")
-                .textColor(Color.RED)
-                .textShadow(0.1f,  5, 5, Color.MAGENTA)
-                .alpha(255)
-                .rotation(-45)
-                .rubberStampPosition(RubberStampPosition.CENTER)
-                .build();
+        mTextColor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mColorPicker.show();
+                mColorPicker.setCallback(new ColorPickerCallback() {
+                    @Override
+                    public void onColorChosen(@ColorInt int color) {
+                        Drawable background = mTextColor.getCompoundDrawables()[2];
+                        GradientDrawable gradientDrawable = (GradientDrawable) background;
+                        gradientDrawable.setColor(color);
+                        mTextColorValue = color;
+                        mColorPicker.dismiss();
+                    }
+                });
+                return false;
+            }
+        });
 
-        Observable<Bitmap> observable = getBitmap(rubberStamp, config);
+        mTextBackgroundColor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mColorPicker.show();
+                mColorPicker.setCallback(new ColorPickerCallback() {
+                    @Override
+                    public void onColorChosen(@ColorInt int color) {
+                        Drawable background = mTextBackgroundColor.getCompoundDrawables()[2];
+                        GradientDrawable gradientDrawable = (GradientDrawable) background;
+                        gradientDrawable.setColor(color);
+                        mTextBackgroundColorValue = color;
+                        mColorPicker.dismiss();
+                    }
+                });
+                return false;
+            }
+        });
+    }
+
+    public void generateRubberStamp() {
+        RubberStampConfig config;
+        int alpha = mAlphaSeekBar.getProgress();
+        int rotation = mRotationSeekBar.getProgress();
+        RubberStampPosition rubberStampPosition =
+                convertToRubberStampPosition(mRubberStampPosition.getSelectedItemPosition());
+
+        if(mRadioGroup.getCheckedRadioButtonId() == R.id.bitmapRubberStamp) {
+            Bitmap logo = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.logo);
+
+            config = new RubberStampConfig.RubberStampConfigBuilder()
+                    .base(mBaseBitmap)
+                    .rubberStamp(logo)
+                    .alpha(alpha)
+                    .rotation(rotation)
+                    .rubberStampPosition(rubberStampPosition)
+                    .build();
+        } else {
+            Shader shader = getShader();
+            config = new RubberStampConfig.RubberStampConfigBuilder()
+                    .base(mBaseBitmap)
+                    .alpha(alpha)
+                    .rotation(rotation)
+                    .rubberStampPosition(rubberStampPosition)
+                    .rubberStamp(mRubberStampText.getText().toString())
+                    .textColor(mTextColorValue)
+                    .textBackgroundColor(mTextBackgroundColorValue)
+                    .textShader(shader)
+                    .textSize(mTextSizeSeekBar.getProgress())
+                    .build();
+        }
+
+
+        Observable<Bitmap> observable = getBitmap(mRubberStamp, config);
         observable.subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
                 .single()
                 .subscribe(new Action1<Bitmap>() {
                     @Override
                     public void call(Bitmap bitmap) {
-                        imageView.setImageBitmap(bitmap);
+                        mImageView.setImageBitmap(bitmap);
                     }
                 }, new Action1<Throwable>() {
                     @Override
@@ -74,6 +190,34 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, throwable.getMessage());
                     }
                 });
+    }
+
+    private RubberStampPosition convertToRubberStampPosition(int selectedItemPosition) {
+        switch (selectedItemPosition) {
+            case 0: return RubberStampPosition.TOP_LEFT;
+
+            case 1: return RubberStampPosition.TOP_CENTER;
+
+            case 2: return RubberStampPosition.TOP_RIGHT;
+
+            case 3: return RubberStampPosition.CENTER_LEFT;
+
+            case 4: return RubberStampPosition.CENTER;
+
+            case 5: return RubberStampPosition.CENTER_RIGHT;
+
+            case 6: return RubberStampPosition.BOTTOM_LEFT;
+
+            case 7: return RubberStampPosition.BOTTOM_CENTER;
+
+            case 8: return RubberStampPosition.BOTTOM_RIGHT;
+
+            case 9: return RubberStampPosition.CUSTOM;
+
+            case 10: return RubberStampPosition.TILE;
+
+            default: return RubberStampPosition.CENTER;
+        }
     }
 
     public Observable<Bitmap> getBitmap(final RubberStamp rubberStamp,
@@ -106,5 +250,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public Shader getShader() {
+        if (mShaderSwitch.isChecked()) {
+            int[] rainbow = {Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA};
+            Shader shader = new LinearGradient(0, 0, 0, 100, rainbow,
+                    null, Shader.TileMode.MIRROR);
+
+            Matrix matrix = new Matrix();
+            matrix.setRotate(90);
+            shader.setLocalMatrix(matrix);
+
+            return shader;
+        }
+        return null;
     }
 }
